1e8ed25216eed835feb32c9d56153fca87518b55
Hitesh Aleriya <hiteshaleriya@gmail.com>
2017-04-22 17:39:35 +0530

    added support for add to homescreen and offline support

--- angular2017/README.md
+++ angular2017/README.md
@@ -1,4 +1,4 @@
-# Supervisor Portal
+# Supervisor Portal (Progressive Web App)
 
 ### Features:
 
@@ -7,6 +7,11 @@
 2. Supervisor can select a worker from the list and assign a task to him/her, the task should have a name and the time for execution.
 
 3. Task assignment is real time so that supervisor can assign a task if and only if worker is idle.
+
+4. Add to homescreen support (Supervisor can install this app as well).
+
+5. Offline support (Supervisor can also access this app without network).
+
 
 ### Demo:
 https://hiteshaleriya.github.io/angular2017/index.html
--- angular2017/src/app.route.js
+++ angular2017/src/app.route.js
@@ -5,4 +5,13 @@ angular.module('portal')
                 templateUrl: 'src/app/components/portal/portal.html',
                 controller: 'PortalController'
             })
+
+        // register the service worker if available
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('./sw.js').then(function(reg) {
+                console.log('Successfully registered service worker', reg);
+            }).catch(function(err) {
+                console.warn('Error whilst registering service worker', err);
+            });
+        }
     });
